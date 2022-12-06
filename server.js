dd8c583f01c13fb56362e047ea2039af3984d6cb
jubaer.sookh <jubaer@sookh.com>
2022-12-06 12:06:22 +0600

    design change

--- server.js
+++ server.js
@@ -49,6 +49,19 @@ app.post('/chat', async(req, res)  => {
   
 });
 
+// error handling for 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// error handling for 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
+
 
 
 app.listen({ port: process.env.PORT || 3000 }, () => {
